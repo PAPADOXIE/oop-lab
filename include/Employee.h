fd32a2c5d2cabc0f1e77bb0ecee1aa43497cfab9
Waleed Butt <waleedb403@gmail.com>
2020-11-20 15:05:46 +0500

    al

--- include/Employee.h
+++ include/Employee.h
@@ -3,6 +3,7 @@ class Employee{
     static int No_of_employee;
     int id;
     int cabin_no;
+    Employee();
     bool assign_cabin(int cab);
     void take_cabin();
 };
--- include/EmployeeList.h
+++ include/EmployeeList.h
@@ -1,1 +1,10 @@
+#pragma once
 #include <../include/Employee.h>
+class EmployeeList(){
+    Employee **List;
+    int max_size;
+    int currentsize;
+    EmployeeList();
+    void add_Employee(Employee);
+    void remove_Employee();
+}
