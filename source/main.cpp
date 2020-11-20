5c6cc737ad527a787ce9bb25659703553b919d74
PAPADOXIE <nofilqasim@gmail.com>
2020-11-20 16:36:40 +0500

    Added messages to main

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,18 @@
+#include <iostream>
 #include "../include/EmployeeList.h"
 #include "../include/CabinList.h"
 
 int main(int argc, char* argv[]){ //argv[1] will be filename
     
+    if(argc == 1){
+        
+    }else if(argc == 2){
+
+    }else{
+        std::cout << "Wrong Usage" << std::endl
+                  << "For file handling demo\nUsage: ./program <filename>" << std::endl
+                  << "Otherwise: ./program" << std::endl;
+        return 0; 
+    }
 
 }
